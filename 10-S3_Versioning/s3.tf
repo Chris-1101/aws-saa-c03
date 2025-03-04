b5e0b1e152c9a4e2464197920e05cc3e925bcbe1
Chris MB <chrismb.noreply@gmail.com>
2025-03-04 19:25:26 +0100

    Add config for the S3 Versioning lesson

---
+++ 10-S3_Versioning/s3.tf
@@ -0,0 +1,63 @@
+
+# █▀ █ █▀▄▀█ █▀█ █░░ █▀▀   █▀ ▀█▀ █▀█ █▀█ ▄▀█ █▀▀ █▀▀   █▀ █▀▀ █▀█ █░█ █ █▀▀ █▀▀ 
+# ▄█ █ █░▀░█ █▀▀ █▄▄ ██▄   ▄█ ░█░ █▄█ █▀▄ █▀█ █▄█ ██▄   ▄█ ██▄ █▀▄ ▀▄▀ █ █▄▄ ██▄ 
+
+# S3 bucket
+resource "aws_s3_bucket" "static_website" {
+  bucket        = "cmb-saa-versioned-static-website"
+  force_destroy = true
+}
+
+# Disable public access block
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls = false
+  ignore_public_acls = false
+  block_public_policy = false
+  restrict_public_buckets = false
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  rule {
+    bucket_key_enabled = true
+
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# S3 Static Website
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Bucket Policy
+resource "aws_s3_bucket_policy" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+  policy = templatefile("json/bucket_policy.json", {
+    bucket_arn = aws_s3_bucket.static_website.arn
+  })
+}
+
+# Bucket Versioning
+resource "aws_s3_bucket_versioning" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
