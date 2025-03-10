bf773318dade0c628f03143f72b82d216ef3d9fc
Chris MB <chrismb.noreply@gmail.com>
2025-03-10 18:33:55 +0100

    Add config for the SSH vs Instance Connect demo

---
+++ 21-EC2_SSH_SSM/ec2.tf
@@ -0,0 +1,21 @@
+
+# █▀▀ █░░ ▄▀█ █▀ ▀█▀ █ █▀▀   █▀▀ █▀█ █▀▄▀█ █▀█ █░█ ▀█▀ █▀▀   █▀▀ █░░ █▀█ █░█ █▀▄ 
+# ██▄ █▄▄ █▀█ ▄█ ░█░ █ █▄▄   █▄▄ █▄█ █░▀░█ █▀▀ █▄█ ░█░ ██▄   █▄▄ █▄▄ █▄█ █▄█ █▄▀ 
+
+# EC2 Instance
+module "ec2_instance" {
+  source = "../00-Shared_Resources/ec2_module"
+
+  # General
+  ec2_name            = "saa-public-ec2"
+  vpc_id              = module.saa_vpc_1.vpc.id
+  subnet_id           = module.saa_vpc_1.subnet_web_a.id
+
+  # SSH / SSM
+  ec2_key_name        = data.aws_key_pair.operations.key_name
+  ssm_session_support = true
+
+  # Public IPs
+  auto_assign_ipv4    = true
+}
+
